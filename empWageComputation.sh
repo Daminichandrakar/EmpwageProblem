0491165d9f83e61f0d5eec3aad79377a86ea651d
Daminichandrakar <damini.2201@gmail.com>
2021-07-28 19:16:26 +0530

    Added:Welcome to empWageProblem Present-Absent

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,2 +1,15 @@
 #! /bin/bash -x
-echo "Welcome to empWageProblem" 
+echo "Welcome to empWageProblem"
+#constant variable
+IS_PRESENT=1
+
+#variable
+randomCheck=$((RANDOM%2))
+
+#selection
+if [ $randomCheck -eq 1 ]
+then 
+      echo "Employe is present"
+else 
+       echo "Employe is absent"
+fi
